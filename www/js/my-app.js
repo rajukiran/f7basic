ae5fd8337ccd951b1b8efb172effd40b1a8a9567
rajukiran <kiranraju.jakka@gmail.com>
2017-02-04 12:33:58 +0530

    Brand New App

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,61 +1,9 @@
 // Initialize app
 var myApp = new Framework7();
 
-var options = {
-  'bgcolor': '#0da6ec',
-  'fontcolor': '#fff'
-}
-
-var $$ = Dom7;
-
-//Initialize & options
-
-
 
 // If we need to use custom DOM library, let's save it to $$ variable:
-
-
-//Define slides
-var welcomescreen_slides = [
-  {
-    id: 'slide0',
-    picture: '<div class="tutorialicon">♥</div>',
-    text: 'Welcome to this tutorial. In the <a class="tutorial-next-link" href="#">next steps</a> we will guide you through a manual that will teach you how to use this app.'
-  },
-  {
-    id: 'slide1',
-    picture: '<div class="tutorialicon">✲</div>',
-    text: 'This is slide 2'
-  },
-  {
-    id: 'slide2',
-    picture: '<div class="tutorialicon">♫</div>',
-    text: 'This is slide 3'
-  },
-  {
-    id: 'slide3',
-    picture: '<div class="tutorialicon">☆</div>',
-    text: 'Thanks for reading! Enjoy this app or go to <a class="tutorial-previous-slide" href="#">previous slide</a>.<br><br><a class="tutorial-close-btn" href="#">End Tutorial</a>'
-  }
-];
-
-var welcomescreen = myApp.welcomescreen(welcomescreen_slides, options);
-
-$$(document).on('click', '.tutorial-close-btn', function () {
-  welcomescreen.close();
-});
-
-$$('.tutorial-open-btn').click(function () {
-  welcomescreen.open();
-});
-
-$$(document).on('click', '.tutorial-next-link', function (e) {
-  welcomescreen.next();
-});
-
-$$(document).on('click', '.tutorial-previous-slide', function (e) {
-  welcomescreen.previous();
-});
+var $$ = Dom7;
 
 // Add view
 var mainView = myApp.addView('.view-main', {
@@ -68,41 +16,6 @@ $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
 
-// $$(document).on('click', '.item-link', function (e) {
-//   console.log("this is kiran");
-//   var formData={'userId':'nareshmangalagiri@gmail.com','password':'1234','enable':'true'};
-//   $$.ajax({
-//     url: 'http://139.59.3.231:8080/SpringWithHibernate/validateLogin.do/',
-//     data: formData,
-//     method: "POST",
-//     dataType: 'json',
-//     contentType: 'application/json',
-//     success: function(data) {
-//       console.log(data);
-//     },
-//     complete: function(xhr, status){
-//       console.log(status);
-//     }
-//   })
-// });
-
-// var formData={"userId":"nareshmangalagiri@gmail.com","password":"1234","enable":"true"};
-
-// self.post = function (params)
-//      {
-//        var deferred = $q.defer();
-//        var config = {
-//          headers: { 'Content-type': 'application/json' },
-//          'dataType': 'json'
-//        };
-//
-//        $http.post(self.url, params, config).success(function (data) {
-//          deferred.resolve(data);
-//        }).error(function (data) {
-//          deferred.reject(data);
-//        });
-//        return deferred.promise;
-//      };
 
 // Now we need to run the code that will be executed only for About page.
 
